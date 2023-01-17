97b3777dc350549477ff27338266e84aea31f783
AlkeshSakhare <akleshsakhare@yahoo.com>
2023-01-17 17:56:21 +0530

    update

--- src/test/java/begineer/ExpeExceptionTest.java
+++ src/test/java/begineer/ExpeExceptionTest.java
@@ -1,6 +1,5 @@
 package begineer;
 
-
 import org.testng.annotations.Test;
 
 public class ExpeExceptionTest {
--- src/test/java/begineer/Priority.java
+++ src/test/java/begineer/Priority.java
@@ -3,11 +3,15 @@ package begineer;
 import org.testng.annotations.Test;
 
 public class Priority {
-
+	/*
+	 * If test priority is not defined explicitly while running multiple cases,
+	 * TestNG assigns all cases with a Default test priority, i.e., zero (0).
+	 */
 	@Test(priority = -1)
 	public void testCaseNegativeOne() {
 		System.out.println("This is testCaseNegativeOne");
 	}
+
 	@Test(priority = 0)
 	public void testCasePriority0() {
 		System.out.println("This is testCasePriority0");
